13c3e0748bede2f386de9f578d5d36d44fb8399a
Vikramjit Singh <vicky.4949@gmail.com>
2023-11-19 16:44:23 +1030

    rate limiter

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,7 +2,8 @@ import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/api";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
 import {
   createTRPCRouter,
   privateProcedure,
@@ -16,6 +17,18 @@ const filterUserForClient = (user: User) => {
     profileImageUrl: user.imageUrl,
   };
 };
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */ 
+  prefix: "@upstash/ratelimit",
+});
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -39,6 +52,11 @@ export const postRouter = createTRPCRouter({
       // });
 
       const authorId = ctx.userId;
+
+      const {success} = await ratelimit.limit(authorId);
+
+      if(!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"});
+      
       const post = await ctx.db.post.create({
         data: {
           authorId,
